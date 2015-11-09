e917a6fad7775e017f13b65055a0391181cc2c36
erasmo07 <erasmomontes07@hotmail.com>
2015-11-09 11:06:39 -0500

    Changed: the controller to show all model in the template

--- app/app/views.py
+++ app/app/views.py
@@ -2,22 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 from collaborators.models import (Facilitator, Mentor,
                                   Judges, Organizer, Sponsor,
-                                  Event)
+                                  Event, Collaborator)
 
 
 def home(request):
-    facilitators = Facilitator.objects.all()
+    facilitators = Facilitator.objects.all()[:1]
     mentors = Mentor.objects.all()
     judges = Judges.objects.all()
     organizer = Organizer.objects.all()
+    collaborators = Collaborator.objects.all()
 
     # sponsors categori
-    diamond = Sponsor.objects.filter(categori__name='diamante').all()
-    platinum = Sponsor.objects.filter(categori__name='platinum').all()
-    gold = Sponsor.objects.filter(categori__name='gold').all()
-    silver = Sponsor.objects.filter(categori__name='silver').all()
-    bronze = Sponsor.objects.filter(categori__name='bronze').all()
-    media_partner = Sponsor.objects.filter(categori__name='media pathner').all()
+    diamond = Sponsor.objects.filter(categori__number_order=1).all()
+    platinum = Sponsor.objects.filter(categori__number_order=2).all()
+    gold = Sponsor.objects.filter(categori__number_order=3).all()
+    silver = Sponsor.objects.filter(categori__number_order=4).all()
+    bronze = Sponsor.objects.filter(categori__number_order=5).all()
+    media_partner = Sponsor.objects.filter(categori__number_order=6).all()
 
     sponsors = {'diamonds': diamond,
                 'platinums': platinum,
@@ -26,13 +27,14 @@ def home(request):
                 'bronzes': bronze,
                 'media_partners': media_partner}
 
-    event = Event.objects.first()
+    event = Event.objects.all()[:1]
 
     content = {'facilitators': facilitators,
                'mentors': mentors,
                'judges': judges,
                'organizer': organizer,
                'sponsors': sponsors,
-               'event': event}
+               'collaborators': collaborators,
+               'event': event[0]}
 
     return render_to_response('base.html', content)
