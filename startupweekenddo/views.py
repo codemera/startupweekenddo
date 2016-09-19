437a4799171e194300a340f3dce781124104afd8
Ricardo Restituyo <ricardo@codemera.com>
2016-09-19 17:02:54 -0400

    Handle missing schedule and banner errors

--- startupweekenddo/views.py
+++ startupweekenddo/views.py
@@ -1,15 +1,21 @@
-from mezzanine.utils.views import render
 from startupweekenddo.models import Event
 from django.db.models import Count, Sum
+from django.template.response import TemplateResponse
 
 
 def index(request):
 
     current_event = Event.objects.published().first()
-    schedule_days = current_event.schedule.scheduleitem_set.all().datetimes('time', 'day')
+    schedule_days = None
+    try:
+        if current_event:
+            schedule_days = current_event.schedule.scheduleitem_set.all().datetimes('time', 'day')
+    except Exception:
+        # Event has no schedule
+        schedule_days = None
 
     context = {
-        'events_count': Event.objects.count(),
+        'events_count': Event.objects.published().count(),
         'participants_count': 0,
         'cities_count': 0,
         'current_event': current_event,
@@ -19,4 +25,4 @@ def index(request):
     context.update(Event.objects.published().aggregate(participants_count=Sum('participants'),
                                                        cities_count=Count("city")))
 
-    return render(request, "index.html", context)
+    return TemplateResponse(request, "index.html", context)
