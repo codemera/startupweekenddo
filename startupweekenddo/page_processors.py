82863db5b1f38537e1ecedd80352c35eba37e5ff
Manuel Tejada <manute32@gmail.com>
2016-09-28 13:21:45 -0400

    Add past events + FAQs to `Startup Weekend` page

--- startupweekenddo/page_processors.py
+++ startupweekenddo/page_processors.py
@@ -1,5 +1,5 @@
 from mezzanine.pages.page_processors import processor_for
-from startupweekenddo.models import Question
+from startupweekenddo.models import Question, Event
 
 
 @processor_for('startup-weekend')
@@ -7,3 +7,9 @@ def add_faq(request, page):
     questions = Question.objects.all()
 
     return {'faq': questions}
+
+@processor_for('startup-weekend')
+def add_events(request, page):
+    events = Event.objects.published()[:3]
+
+    return {'past_events': events}
