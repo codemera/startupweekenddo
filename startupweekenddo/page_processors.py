0510293515396a8a77c6015b2a1a39992d1880e0
Ricardo Restituyo <ricardo@codemera.com>
2016-09-29 11:41:16 -0400

    Add categories for FAQ questions

--- startupweekenddo/page_processors.py
+++ startupweekenddo/page_processors.py
@@ -1,9 +1,14 @@
 from mezzanine.pages.page_processors import processor_for
-from startupweekenddo.models import Question
+from startupweekenddo.models import QuestionCategory
+from collections import OrderedDict
 
 
 @processor_for('startup-weekend')
 def add_faq(request, page):
-    questions = Question.objects.all()
+    cats = QuestionCategory.objects.all()
+    questions = OrderedDict()
+
+    for cat in cats:
+        questions[cat.name] = cat.questions.all()
 
     return {'faq': questions}
