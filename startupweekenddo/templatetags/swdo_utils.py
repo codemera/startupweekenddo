491b151680e243ab3c8aec9f95d00ff22655acdf
Manuel Tejada <manute32@gmail.com>
2016-09-30 15:34:41 -0400

    Create social_links templatetag

--- startupweekenddo/templatetags/swdo_utils.py
+++ startupweekenddo/templatetags/swdo_utils.py
@@ -1,6 +1,17 @@
 from django.template.defaulttags import register
+from django import template
 
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.simple_tag(takes_context=True)
+def social_links(context, extra_class=None):
+    template_name = 'widgets/social-links.html'
+
+    context = {
+        'extra_class': extra_class,
+    }
+    return template.loader.get_template(template_name).render(template.Context(context))
