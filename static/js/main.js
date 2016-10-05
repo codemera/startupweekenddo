34fa43226db5d84d9641eb8bc0b0693150a3587e
Manuel Tejada <manute32@gmail.com>
2016-10-05 10:03:23 -0400

    Add Instagram feed

--- static/js/main.js
+++ static/js/main.js
@@ -27,4 +27,14 @@ $(function(){
       }
     });
   });
+
+  // Instafeed initializer
+  var feed = new Instafeed({
+		get: 'user',
+		clientId: '694f51e4c5964fdca7df9a493fcea120',
+		accessToken: '3203931.694f51e.05905d3d8be4472b99a9c92b20988c0e',
+		userId: 1542077877,
+		limit: 12
+	});
+	feed.run();
 });
